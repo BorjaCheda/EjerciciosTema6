3eb4d7af458b4a90792a878bede9d00ffe01f81b
BorjaChedaCandales <borja.cheda@nunegal.com>
2024-03-19 15:54:03 +0100

    Igual que el anterior pero aquí vamos a añadir la gestión de usuarios creados dinámicamente y almacenados en base de datos, no en memoria.

---
+++ bcc0901b/src/test/java/com/chedacandalesborja/bcc0601/Bcc0601ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chedacandalesborja.bcc0601;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bcc0601ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
